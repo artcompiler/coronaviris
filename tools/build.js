1d586b4d443964a829bfedc0ae0495d78d1b4636
Jeff Dyer <jeffdyer@acm.org>
2020-03-26 09:07:38 -0700

    checkpoint

--- tools/build.js
+++ tools/build.js
@@ -77,7 +77,7 @@ function generate() {
         obj.values.push([date, value]);
         total += value;
       });
-      if (total > 1) {
+      if (total > 100) {
         data.push({
           id: CHART_ID,
           data: obj
@@ -137,6 +137,7 @@ function putComp(secret, data, resume) {
       data += chunk;
     }).on('end', function () {
       if (resume) {
+        console.log("putComp() data=" + JSON.stringify(data));
         resume(null, JSON.parse(data));
       }
     }).on("error", function (err) {
@@ -150,6 +151,45 @@ function putComp(secret, data, resume) {
   });
 }
 
+function postSnap(id, resume) {
+  let encodedData = JSON.stringify({
+    id: id,
+    host: "https://gc.acx.ac", // coronavirus.artcompiler.com, c9s.acx.ac
+  });
+  var options = {
+    host: "puppeteer-service.artcompiler.com",
+    port: "80",
+    path: "/snap",
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(encodedData),
+    },
+  };
+  var req = http.request(options);
+  req.on("response", (res) => {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    }).on('end', function () {
+      if (res.statusCode !== 200) {
+        resume(res.statusCode);
+      } else {
+        resume(null, data);
+      }
+    }).on("error", function () {
+      console.log("error() status=" + res.statusCode + " data=" + data);
+      resume(res.statusCode);
+    });
+  });
+  req.end(encodedData);
+  req.on('error', function(err) {
+    console.log("[12] ERROR " + err);
+    resume(err);
+  });
+}
+
+
 function compile(data) {
   const secret = process.env.ARTCOMPILER_CLIENT_SECRET;
   putComp(secret, data, (err, val) => {
@@ -158,10 +198,19 @@ function compile(data) {
     pageStr += 'row twelve-columns [br, ';
     pageStr += 'style { "fontSize": "10"} cspan "Posted: ' + date + '"';
     pageStr += '],\n';
+    let completed = 0;
     val && val.length && val.forEach((v, i) => {
-      console.log("compile() v=" + JSON.stringify(v, null, 2));
+//      console.log("compile() v=" + JSON.stringify(v, null, 2));
       pageStr +=
-        'row twelve-columns [br, cspan "' + v.data.args.region + '", href "item?id=' + v.id + '" style { \"height\": 200} graffito "' + v.id + '"]\n';
+        'row twelve-columns [br, cspan "' + v.data.args.region +
+        '", href "item?id=' + v.id +
+        '" img "https://cdn.acx.ac/' + v.id + '.png' +
+//        '" style { \"height\": 200} graffito "' + v.id +
+        '"]\n';
+      postSnap(v.id, (err, data) => {
+        completed++;
+        console.log(completed + " of " + val.length + " charts compiled");
+      });
     });
     pageStr += "]..";
     console.log("compile() pageStr=" + pageStr);
