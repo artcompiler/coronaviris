fc1d6d79c88e08df56832f17a348ca14e5830b45
Jeff Dyer <jeffdyer@acm.org>
2020-05-01 14:22:58 -0700

    refresh

--- tools/build.js
+++ tools/build.js
@@ -47,10 +47,10 @@ function clean() {
 }
 
 const FILES = [
-//  '../build/data/nyt-us.json',
-//  '../build/data/nyt-us-states.json',
-  '../build/data/usafacts-us-counties.json',
-  '../build/data/isciii-spain.json',
+  '../build/data/nyt-us.json',
+  '../build/data/nyt-us-states.json',
+//  '../build/data/usafacts-us-counties.json',
+//  '../build/data/isciii-spain.json',
 ];
 const SCALE = 5; //FILES.length;
 
