8c12317189f586fbaefbb2d763541cf3933bab53
Jeff Dyer <jeffdyer@acm.org>
2020-05-10 08:37:08 -0700

    refresh

--- tools/build.js
+++ tools/build.js
@@ -48,10 +48,10 @@ function clean() {
 
 const FILES = [
   // '../build/data/owid-world.json',
-  '../build/data/usafacts-us-counties.json',
-  // '../build/data/nyt-us.json',
-  // '../build/data/nyt-us-states.json',
-  // '../build/data/nyt-us-counties.json',
+  // '../build/data/usafacts-us-counties.json',
+  '../build/data/nyt-us.json',
+  '../build/data/nyt-us-states.json',
+  '../build/data/nyt-us-counties.json',
 ];
 const SCALE = 5; //FILES.length;
 
@@ -75,7 +75,7 @@ const DEATHS_CHART_ID = "l16CBlVv2fX";
 const CASES_CHART_ID = "Je1cxWV8bsx";
 const RECOVERED_CHART_ID = "";
 
-const THRESHOLD = 20;
+const THRESHOLD = 10;
 
 const pingCache = {};
 function pingLang(lang, resume) {
@@ -365,8 +365,8 @@ function generate(file) {
     for (let i = objNewDeathsValues.length - 1; i > objNewDeathsValues.length - 8; i--) {
       sevenDayDeathTotal += objNewDeathsValues[i][1];
     }
-    if (+row.deaths[prevDate] >= THRESHOLD) {
-    // if (sevenDayDeathTotal >= THRESHOLD) {
+    //if (+row.deaths[prevDate] >= THRESHOLD) {
+    if (sevenDayDeathTotal >= THRESHOLD) {
       if (!data[parent]) {
         data[parent] = {
           dataSource: dataSource,
