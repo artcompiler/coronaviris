b6ecc963f2918b594672589aa469e998eff65e6a
Jeff Dyer <jeffdyer@acm.org>
2020-09-11 12:37:57 -0700

    -

--- tools/build.js
+++ tools/build.js
@@ -51,7 +51,7 @@ const FILES = [
   // '../build/data/nyt-us-states.json',
   // '../build/data/nyt-us-counties.json',
   '../build/data/owid-world.json',
-  // '../build/data/usafacts-us-counties.json',
+  '../build/data/usafacts-us-counties.json',
 ];
 const SCALE = 5; //FILES.length;
 
