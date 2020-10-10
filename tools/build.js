6146df8c797610cedfc8a28a7bb94f4065f7366f
Jeff Dyer <jeffdyer@acm.org>
2020-10-10 09:04:10 -0700

    -

--- tools/build.js
+++ tools/build.js
@@ -47,9 +47,9 @@ function clean() {
 }
 
 const FILES = [
-  // '../build/data/nyt-us.json',
-  // '../build/data/nyt-us-states.json',
-  // '../build/data/nyt-us-counties.json',
+  '../build/data/nyt-us.json',
+  '../build/data/nyt-us-states.json',
+  '../build/data/nyt-us-counties.json',
   '../build/data/owid-world.json',
   // '../build/data/usafacts-us-counties.json',
 ];
